b1dd205c5d1e970a0a836d5512edac4bf2b54f1b
shibafu <shibafu528@gmail.com>
2019-11-28 01:56:12 +0900

    抵抗しても勧める

--- kadoma.rb
+++ kadoma.rb
@@ -1,7 +1,7 @@
 Plugin.create :kadoma do
   on_update do |w, ms|
     ms.each do |m|
-      if m.description.include?("門真")
+      if m.description.include?("門真") || (m.description.include?("@") && m.description.include?("いやじゃ"))
         compose(w, body: "@toshi_a@social.mikutter.hachune.net まちカドまぞくを見ろ")
       end
     end
